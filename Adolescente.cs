56ac8cbd3b62377ea0981925734abaf7d6fd7536
Oscar García <54967287+OscarGarcia98@users.noreply.github.com>
2019-10-24 10:48:44 -0700

    Add files via upload

---
+++ Adolescente.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepoPeso
+{
+    public class Adolescente : Persona
+    {
+        public override int Id { get; set; }
+        public override string NoTel { get; set; }
+        public override string Nombre { get; set; }
+        public override string Direccion { get; set; }
+        public override string Email { get; set; }
+        public override string Peso { get; set; }
+
+        public override string Obtener_Nombre(Persona p)
+        {
+            return p.Nombre + " : "+ p.NoTel;
+        }
+        public override void Mostrar_Datos(Persona p)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
---
+++ Persona.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace RepoPeso      //una librería de clases contiene codigo el cual se puede utilizar en otros proyectos
+{
+    public abstract class Persona
+    {
+        public abstract int Id { get; set; }    
+        public abstract string NoTel { get; set; }
+        public abstract string Nombre { get; set; }
+        public abstract string Email { get; set; }
+        public abstract string Direccion { get; set; }
+        public abstract string Peso { get; set; }
+
+
+
+        public abstract void Mostrar_Datos(Persona p);      //los metodos y atributos abstractos se deben declarar en las clases heredadas
+        public abstract string Obtener_Nombre(Persona p);
+    
+
+
+    }
+}
